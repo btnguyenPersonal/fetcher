958e96ba10760722716d54097bbedf29e4ef0ba6
btnguyenPersonal <ben706519@gmail.com>
2025-09-24 18:31:24 -0500

    wip authorizationCode

---
+++ authorizationCode.ts
@@ -0,0 +1,10 @@
+export async function authorizationCode(config: {
+    authorizationUrl: string,
+    accessTokenUrl: string,
+    clientId: string,
+    clientSecret: string,
+    redirectUrl: string,
+    scope: string,
+}, debug?: boolean) {
+    // TODO hard bruh
+}
